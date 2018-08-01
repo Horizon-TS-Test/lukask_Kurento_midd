118d7c41b3f74a79c02ea695491a008e31c6d7a5
dennysmoyon <dmoyon@horizon-ts.com>
2018-08-01 16:12:36 -0500

    servidores de kurento

--- config/kurento_server.js
+++ config/kurento_server.js
@@ -1,4 +1,4 @@
 module.exports = {
     //kurentoServer_url_ws : "ws://192.168.1.48:8888/kurento"
-    kurentoServer_url_ws : "ws://ec2-18-231-172-83.sa-east-1.compute.amazonaws.com:8888/kurento"
+    kurentoServer_url_ws : "ws://ec2-18-231-113-8.sa-east-1.compute.amazonaws.com:8888/kurento"
 }
